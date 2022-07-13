fe7d2fb3bf02fde1bf8aa71312b69fc18aa81f99
hemant935 <82675433+hemant935@users.noreply.github.com>
2022-07-13 22:36:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,4 +29,4 @@ mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true})
 .then(()=>app.listen(PORT,()=>console.log("Server running on port:"+PORT)))
 .catch((error)=>console.log(error.message));
 
-export default 0;
+export default app;
